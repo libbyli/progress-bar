0cd18f80009ea2c77ddeffa9e20853978da07b99
Elizabeth Li <sincerelylibby@gmail.com>
2018-09-03 13:37:33 -0700

    Add functionality for unfilling the bar and both buttons.

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let count = 0;
 
 const changeBar = (button) => {
   let bar = document.getElementsByClassName('bar');
-  bar[count].style.background = 'green';
-  count += 1;
+  if (button === 'next') {
+    bar[count].style.background = 'green';
+    count += 1;
+  } else {
+    count -= 1;
+    bar[count].style.background = 'white';
+  }
 };
